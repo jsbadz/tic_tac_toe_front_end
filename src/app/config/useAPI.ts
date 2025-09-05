4476e4ef5f78fe400b1ee69f8f8afa8038b363aa
johnstephen-ai <johnstephen@ajio.co.jp>
2025-09-06 00:31:18 +0800

    env error

--- src/app/config/useAPI.ts
+++ src/app/config/useAPI.ts
@@ -1,9 +1,5 @@
-export const useApiUrl =
-  process.env.NODE_ENV === "development"
-    ? process.env.NEXT_PUBLIC_API_URL_LOCAL!
-    : process.env.NEXT_PUBLIC_API_URL_PROD!;
+export const useApiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-// Optional runtime check
 if (!useApiUrl) {
   throw new Error("API URL is not defined in environment variables");
 }
